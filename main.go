b85829a1cbbc6564ab95765ad6e5406faf282160
Kaji <jiomamaoag@gmail.com>
2022-04-27 01:44:56 +0800

    Setup scanner

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func readFile(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func scanEndpoint(url string, path string) uint8 {
+
+	resp, err := http.Get(fmt.Sprintf("https://%s%s", url, path))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if resp.StatusCode == 404 {
+		log.Printf("%d %d: GET %s: Status - %d\n", log.Ldate, log.Ltime, path, resp.StatusCode)
+		return 0
+	}
+
+	log.Printf("%s %d %d: GET %s: Status - %d%s\n", "\033[32m", log.Ldate, log.Ltime, path, resp.StatusCode, "\033[0m")
+	return 1
+}
+
+func main() {
+	const APPNAME string = "Binoscan"
+	var baseUrl string
+	var resultsFound uint8 = 0
+
+	fmt.Printf("%s - an api application scanner.\n", APPNAME)
+	fmt.Print("Enter your base url: ")
+
+	fmt.Scan(&baseUrl)
+
+	paths, err := readFile("api-wordlist.txt")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, path := range paths {
+		resultsFound = resultsFound + scanEndpoint(baseUrl, path)
+	}
+
+	fmt.Printf("Scanning Complete. There are %d routes are available \n", resultsFound)
+}
